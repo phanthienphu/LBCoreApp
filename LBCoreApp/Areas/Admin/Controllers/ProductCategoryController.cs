d4162559e3c7cab8d11fa9e6658e10c3617e22c0
Phan Thien Phu <phanthienphu199@gmail.com>
2018-09-28 00:57:59 +0700

    (20180927)_create treeview for productcategory with easyui

---
+++ LBCoreApp/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LBCoreApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LBCoreApp.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : BaseController
+    {
+        IProductCategoryService _productCategoryService;
+        public ProductCategoryController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Get Data API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var model = _productCategoryService.GetAll();
+            return new OkObjectResult(model);
+        }
+        #endregion
+    }
+}
---
+++ LBCoreApp/wwwroot/app/Controllers/Product-Category/Index.js
@@ -0,0 +1,31 @@
+﻿var ProductCategoryController = function(){
+    this.initialize = function () {
+        LoadData();
+    }
+
+    function LoadData() {
+        $.ajax({
+            url: '/Admin/ProductCategory/GetAll',
+            dataType: 'json',
+            success: function (response) {
+                var data = [];
+                $.each(response, function (i, item) {
+                    data.push({
+                        id: item.id,
+                        text: item.Name,
+                        parentId: item.ParentId,
+                        sortOrder: item.SortOrder
+                    });
+                });
+                var treeArr = common.unflattern(data);
+
+                //var $tree = $('#treeProductCategory');
+
+                $('#treeProductCategory').tree({
+                    data: treeArr,
+                    dnd: true
+                });
+            }
+        });
+    }
+}
